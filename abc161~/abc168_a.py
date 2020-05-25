f556fbcfb4df0c90f61c45b2fc8d3cbcbd47c101
Shirohi-git <mario1983@mbr.nifty.com>
2020-05-26 02:06:24 +0900

    small change

--- abc161~/abc168_a.py
+++ abc161~/abc168_a.py
@@ -1,7 +1,7 @@
 n = int(input()[-1])
-if n == 2 or n == 4 or n == 5 or n == 7 or n == 9:
-    print('hon')
-elif n == 0 or n == 1 or n == 6 or n == 8:
+if n == 3:
+    print('bon')
+elif n in set([0, 1, 6, 8]):
     print('pon')
 else:
-    print('bon')
+    print('hon')
