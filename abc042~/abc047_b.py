b28294b4d64445410c17e91481748993b6805a85
Shirohi-git <mario1983@mbr.nifty.com>
2020-06-02 22:27:01 +0900

    daily mission

---
+++ abc042~/abc047_b.py
@@ -0,0 +1,14 @@
+w, h, n = map(int, input().split())
+xya = [map(int, input().split()) for _ in range(n)]
+
+l, r, b, u = 0, w, 0, h
+for x, y, a in xya:
+    if a == 1:
+        l = max(x, l)
+    elif a == 2:
+        r = min(x, r)
+    elif a == 3:
+        b = max(y, b)
+    elif a == 4:
+        u = min(y, u)
+print(max(0,r - l) * max(0,u - b))
