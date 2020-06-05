363675e2af32e759846d7291a5f062a0c597d0c7
Shirohi-git <mario1983@mbr.nifty.com>
2020-06-06 01:20:18 +0900

    daily mission

---
+++ workbench/agc022_aX.py
@@ -0,0 +1,20 @@
+s = str(input())
+
+if s == 'zyxwvutsrqponmlkjihgfedcba':
+    print(-1)
+elif len(s) == 26:
+    tmp = ord(s[-1])
+    for i in range(len(s))[::-1]:
+        if ord(s[i]) > tmp:
+            ans = sorted(s[i - 2:])
+            for j in ans:
+                if ord(j) > ord(s[i - 2]):
+                    print(s[:i - 2] + j)
+                    exit()
+        tmp = ord(s[i])
+else:
+    alpha = [chr(i) for i in range(97, 97 + 26)]
+    for i in alpha:
+        if i not in s:
+            print(s + i)
+            break
