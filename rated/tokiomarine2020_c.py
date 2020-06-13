f1a730be2d8d35c4b53e4ddba02d715ec2c141c3
Shirohi-git <mario1983@mbr.nifty.com>
2020-06-14 03:01:42 +0900

    small change

--- rated/tokiomarine2020_c.py
+++ rated/tokiomarine2020_c.py
@@ -4,12 +4,12 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
 for j in range(k):
-    a01 = [0] * (n + 1)
+    acc = [0] * (n + 1)
     for i, ai in enumerate(a):
         l, r = max(0, i - ai), min(i + ai + 1, n)
-        a01[l] += 1
-        a01[r] -= 1
-    a = list(accumulate(a01))
-    if all(ai == n for ai in a[:-1]):
+        acc[l] += 1
+        acc[r] -= 1
+    a = list(accumulate(acc))[:-1]
+    if all(ai == n for ai in a):
         break
-print(*a[:-1])
+print(*a)
