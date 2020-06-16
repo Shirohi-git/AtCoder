65669a87b8c0768e2f71964940722761fd732e59
Shirohi-git <mario1983@mbr.nifty.com>
2020-06-16 18:08:59 +0900

    small change

--- workbench/.vscode/test.sh
+++ workbench/.vscode/test.sh
@@ -1,13 +1,13 @@
 sh ../../secret/s_AtCoder/login.sh
 
 problem_name=$1
-test_dir=test/${problem_name}
+test_dir=${problem_name}
 base_url=${problem_name%_*}
 url=https://atcoder.jp/contests/${base_url}/tasks/${problem_name}
 
 # make test directory
 if [ ! -e ${test_dir} ]; then
-    oj dl -d test/${problem_name} $url
+    oj dl -d ${problem_name} $url
 fi
 
-oj test -c "python3 ${problem_name}.py" -d test/${problem_name}
+oj test -c "python3 ${problem_name}.py" -d ${problem_name}
