3466a6da4b63c35e3114d0d9ddacd010a9fefd4b
Shirohi-git <mario1983@mbr.nifty.com>
2020-06-27 00:37:17 +0900

    small change

--- abc001-/abc006_2.py
+++ abc001-/abc006_2.py
@@ -1,6 +1,8 @@
 n = int(input())
+mod = 10007
 
-a1, a2, a3 = 0, 0, 1
-for i in range(n - 1):
-    a1, a2, a3 = a2, a3, (a1 + a2 + a3) % 10007
-print(a1)
+tri = [0] * 10 ** 6
+tri[2] = 1
+for i in range(3, n):
+    tri[i] = (tri[i - 1] + tri[i - 2] + tri[i - 3]) % mod
+print(tri[n - 1])
--- arc001-/arc051_b.py
+++ arc001-/arc051_b.py
@@ -1,15 +1,8 @@
-def fibonacci(n, FIBO):
-    if FIBO[n - 1] > 0:
-        return FIBO[n-1]
-    elif n <= 2:
-        FIBO[n - 1] = 1
-        return FIBO[n - 1]
-    else:
-        FIBO[n - 1] = fibonacci(n - 1, FIBO) + fibonacci(n - 2, FIBO)
-        return FIBO[n - 1]
-
 k = int(input())
 
 fibo = [0] * (k + 2)
-fibonacci(k + 2, fibo)
+fibo[0], fibo[1] = 1, 1
+for i in range(2, k + 2):
+    fibo[i] = fibo[i - 1] + fibo[i - 2]
+
 print(fibo[k + 1], fibo[k])
