237c00b3a120a848d9e6a036725a1e7d2ed47679
Shirohi-git <mario1983@mbr.nifty.com>
2020-07-09 01:05:00 +0900

    virtual contest

---
+++ abc141-/abc145_d.py
@@ -0,0 +1,19 @@
+def bigcmb(N, R, MOD):  # nCr(mod p) #n>=10**7,r<=10**6 #前処理不要
+    if (R < 0) or (N < R):
+        return 0
+    R = min(R, N - R)
+    fact, inv = 1, 1
+    for i in range(1, R + 1):
+        fact = (fact * (N - i + 1)) % MOD
+        inv = (inv * i) % MOD
+    return fact * pow(inv, MOD - 2, MOD) % MOD
+
+
+x, y = map(int, input().split())
+mod = 10 ** 9 + 7
+
+if (x + y) % 3 != 0 or (x > 2 * y) or (2 * x < y):
+    print(0)
+else:
+    cnt = (x + y) // 3
+    print(bigcmb(cnt, x - cnt, mod))
