567e3c99128b21c56a02e7fb782ac34b7169beb5
Shirohi-git <mario1983@mbr.nifty.com>
2020-07-12 01:35:36 +0900

    aising_d

--- rated/aising2020_d.py
+++ rated/aising2020_d.py
@@ -8,18 +8,19 @@ def bitcount(N):  # 立ってるbitの数
 n, x = int(input()), input()
 
 num, cnt = int(x, 2), x.count('1')
+num0, num1 = num % (cnt + 1), num % max(cnt - 1, 1)
 lbit = bitcount(18)
 for i in range(n):
     ans = 1
     if x[i] == '0':
         ctmp = cnt + 1
-        tmp = num % ctmp + pow(2, n - (i + 1), ctmp)
+        tmp = num0 + pow(2, n - (i + 1), cnt + 1)
     elif x[i] == '1':
         if cnt == 1:
             print(0)
             continue
         ctmp = cnt - 1
-        tmp = num % ctmp - pow(2, n - (i + 1), ctmp)
+        tmp = num1 - pow(2, n - (i + 1), cnt - 1)
     tmp %= ctmp
     ctmp = lbit[tmp]
     while tmp > 0:
