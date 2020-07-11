4474f3cb799a2ded5c874f2f5542bcb679306201
Shirohi-git <mario1983@mbr.nifty.com>
2020-07-11 20:42:41 +0900

    delete file

--- workbench/aising2020_a.py
+++
@@ -1,3 +0,0 @@
-s = input()
-n = map(int, input().split())
-a = list(map(int, input().split()))
--- workbench/aising2020_e.py
+++
@@ -1,7 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-s = input()
-n = map(int, input().split())
-a = list(map(int, input().split()))
-ab = [list(map(int, input().split())) for _ in range(n)]
