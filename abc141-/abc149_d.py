6fe2fd1e01f7d2d49da68118f6ea9673d3c2bffe
Shirohi-git <mario1983@mbr.nifty.com>
2020-08-02 20:51:25 +0900

    small change

--- abc141-/abc149_d.py
+++ abc141-/abc149_d.py
@@ -2,12 +2,11 @@ n, k = map(int, input().split())
 r, s, p = map(int, input().split())
 t = input()
 
+dict = {'s': r, 'p': s, 'r': p}
 cnt, ans = set(), 0
 for i, ti in enumerate(t):
     if (i - k in cnt) and ti == t[i - k]:
         continue
-    ans += r * (ti == 's')
-    ans += s * (ti == 'p')
-    ans += p * (ti == 'r')
+    ans += dict[ti]
     cnt.add(i)
 print(ans)
