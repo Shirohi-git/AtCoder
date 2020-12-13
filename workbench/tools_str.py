f0e0d1e63cb2bfa2e5f70dcc8433d666ee4311ca
Shirohi-git <mario1983@mbr.nifty.com>
2020-12-13 23:25:18 +0900

    add levenshtein

--- workbench/tools_str.py
+++ workbench/tools_str.py
@@ -14,7 +14,7 @@ def Z_algo(S):  # 最長共通接頭辞 S:文字列
     return z
 
 
-def lcs(S, T):  # 最長共通部分列 # S,T:文字列
+def lcs(S, T):  # 最長共通部分列  # S,T:文字列
     ls, lt = len(S), len(T)
     dp = [[0] * (lt + 1) for _ in range(ls + 1)]
     dp[0][0], dp[1][0], dp[0][1] = 0, 0, 0
@@ -23,4 +23,21 @@ def lcs(S, T):  # 最長共通部分列 # S,T:文字列
             dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
             if S[i] == T[j]:
                 dp[i + 1][j + 1] = max(dp[i][j] + 1, dp[i + 1][j + 1])
+    return dp[ls][lt]
+
+
+def levenshtein(S, T):  # レーベンシュタイン距離  # S,T:文字列
+    ls, lt = len(S), len(T)
+    dp = [[0] * (lt + 1) for _ in range(ls + 1)]
+    for i in range(ls + 1):
+        dp[i][0] = i
+    for j in range(lt + 1):
+        dp[0][j] = j
+
+    for i in range(1, ls + 1):
+        for j in range(1, lt + 1):
+            cost = 0 if S[i - 1] == T[j - 1] else 1
+            dp[i][j] = min(dp[i - 1][j] + 1,         # insertion
+                           dp[i][j - 1] + 1,         # deletion
+                           dp[i - 1][j - 1] + cost)  # replacement
     return dp[ls][lt]
