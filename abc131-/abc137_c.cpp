c88e48b88947b1fa85a479dfdefb21269df0d2ab
Shirohi-git <mario1983@mbr.nifty.com>
2021-05-03 18:18:13 +0900

    daily mission

--- abc131-/abc137_c.cpp
+++ abc131-/abc137_c.cpp
@@ -2,19 +2,27 @@
 using namespace std;
 using ll = long long;
 
-#define min(v) *min_element(v.begin(), v.end())
-#define max(v) *max_element(v.begin(), v.end())
 #define sort_all(s) sort(s.begin(), s.end())
 #define rep(i, n) for (int i = 0; i < int(n); i++)
-#define repi(a, b, i) for (int i = int(a); i < int(b); i++)
 #define for_itr(id, itr) for (auto&& id : itr)
 #define for_dic(key, val, dic) for (auto&& [key, val] : dic)
 
 int main() {
     ll n;
     cin >> n;
-    vector<ll> a(n);
-    rep(i, n) { cin >> a[i]; }
+    vector<string> s(n);
+    rep(i, n) { cin >> s[i]; }
 
+    map<string, ll> dict;
+    for_itr(si, s) {
+        sort_all(si);
+        if (dict.count(si))
+            dict[si]++;
+        else
+            dict[si] = 1;
+    }
+    ll ans = 0;
+    for_dic(k, v, dict) ans += v * (v - 1) / 2;
+    cout << ans << endl;
     return 0;
 }
