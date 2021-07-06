5e3bf97e6f85ece3b2ce583adfdfa7f137eb2c17
Shirohi-git <mario1983@mbr.nifty.com>
2021-07-07 01:43:32 +0900

    make corrections

--- typical90/typical90_aw.py
+++ typical90/typical90_aw.py
@@ -1,1 +1,42 @@
-from heapq import heappush, heappop, heapifydef prim(n0, near0):    flag = [0] * n0    flag[0] = 1    que = [(c, j, 0) for j, c in near0[0]]    heapify(que)    res = 0    while que:        c_pq, q, p = heappop(que)        if flag[q]:            continue        flag[q] = 1        res += c_pq        for r, c_qr in near0[q]:            if 1 - flag[r]:                heappush(que, (c_qr, r, q))    return res if sum(flag) == N+1 else -1def main():    def weighted_nearlist(n0, lst0):        res = [[] for _ in range(n0)]        for c, a, b in lst0:            res[a - 1].append((b, c))            res[b].append((a - 1, c))        return res        near = weighted_nearlist(N+1, CLR)    ans = prim(N+1, near)    return print(ans)if __name__ == '__main__':    N, M = map(int, input().split())    CLR = [map(int, input().split()) for _ in range(M)]    main()    
+from heapq import heappush, heappop, heapify
+
+
+def prim(n0, near0):
+    flag = [0] * n0
+    flag[0] = 1
+    que = [(c, j, 0) for j, c in near0[0]]
+    heapify(que)
+
+    res = 0
+    while que:
+        c_pq, q, p = heappop(que)
+        if flag[q]:
+            continue
+        flag[q] = 1
+        res += c_pq
+        for r, c_qr in near0[q]:
+            if 1 - flag[r]:
+                heappush(que, (c_qr, r, q))
+    return res if sum(flag) == N+1 else -1
+
+
+def main():
+
+    def weighted_nearlist(n0, lst0):
+        res = [[] for _ in range(n0)]
+        for c, a, b in lst0:
+            res[a - 1].append((b, c))
+            res[b].append((a - 1, c))
+        return res
+    
+    near = weighted_nearlist(N+1, CLR)
+    ans = prim(N+1, near)
+    return print(ans)
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    CLR = [map(int, input().split()) for _ in range(M)]
+
+    main()
+    
