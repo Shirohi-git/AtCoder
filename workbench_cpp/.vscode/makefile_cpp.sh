65f7335456784331da50cd24a4738c68e103aaf7
Shirohi-git <mario1983@mbr.nifty.com>
2021-08-01 21:27:47 +0900

    improve makefile

--- workbench_cpp/.vscode/makefile_cpp.sh
+++ workbench_cpp/.vscode/makefile_cpp.sh
@@ -1,3 +1,12 @@
 read name
-cp .vscode/original.cpp ${name}.cpp
-code ${name}.cpp
+if [ `echo ${name} | grep '_'` ]; then
+    cp .vscode/original.cpp ${name}.cpp
+    code ${name}.cpp
+else
+    cp .vscode/original.cpp ${name}_a.cpp
+    cp .vscode/original.cpp ${name}_b.cpp
+    cp .vscode/original.cpp ${name}_c.cpp
+    cp .vscode/original.cpp ${name}_d.cpp
+    cp .vscode/original.cpp ${name}_e.cpp
+    code ${name}_a.cpp
+fi
--- workbench_py/.vscode/makefile_py.sh
+++ workbench_py/.vscode/makefile_py.sh
@@ -1,3 +1,12 @@
 read name
-cp .vscode/original.py ${name}.py
-code ${name}.py
+if [ `echo ${name} | grep '_'` ]; then
+    cp .vscode/original.py ${name}.py
+    code ${name}.py
+else
+    cp .vscode/original.py ${name}_a.py
+    cp .vscode/original.py ${name}_b.py
+    cp .vscode/original.py ${name}_c.py
+    cp .vscode/original.py ${name}_d.py
+    cp .vscode/original.py ${name}_e.py
+    code ${name}_a.py
+fi
